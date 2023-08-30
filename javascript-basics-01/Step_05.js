b7836bacab02c3207adddf18fd2853f8fe482675
ahmadjrdeveloper <ahmadsaad200235@gmail.com>
2023-08-30 12:48:45 +0300

    Step 05 Done!

---
+++ javascript-basics-01/Step_05.js
@@ -0,0 +1,4 @@
+let num1 = prompt("Enter the First number ");
+let num2 = prompt("Enter the Second number ");
+x = num1 * num2;
+alert("The multiplication of " + num1 + " and " + num2 + " is " + x);
