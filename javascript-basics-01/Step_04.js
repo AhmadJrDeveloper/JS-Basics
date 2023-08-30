de3a89ab59428d869cd0a5281fbdc89ee75409a8
ahmadjrdeveloper <ahmadsaad200235@gmail.com>
2023-08-30 12:45:51 +0300

    Step 04 Done!

---
+++ javascript-basics-01/Step_04.js
@@ -0,0 +1,4 @@
+let surName = prompt("Enter your Surname please: ");
+let name = prompt("Enter your Name please: ");
+let city = prompt("Enter your City please: ");
+alert("Surname: " + surName + "\nName: " + name + "\nCity: " + city);
